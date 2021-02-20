f66dd4f39070b6e102d02c5b4ea86f73ee7a75f5
hossam-khalaf <hossamhassan303089@gmail.com>
2021-02-20 21:45:51 +0200

    initial

---
+++ README.md
@@ -0,0 +1,19 @@
+## Control Flow
+
+### conditional statments
+
+- if - else statments
+  - the concept of condition -- truthy and falsy values
+  - and && or
+- Loops =>> for loop
+  the variable should be iterable ==> iteration
+
+  - range()
+
+## dictionry
+
+    - methods
+
+## While loop
+
+- Iterator || Counter
---
+++ aa.py
@@ -0,0 +1,124 @@
+# if  condition == true :
+      # code to be run
+
+# print(bool(' '))
+
+
+# cars = ['bmw', 'ferrari', 'mercedes', 'jeep']
+
+# for car in cars:
+#   if len(car) > 3 and len(car) != 4: #true
+#     print(f'{cars.index(car) } - {car}')
+#   elif len(car) == 4:
+#     print('4 ya 3am el7ag')
+
+# python program to check positive or negative numbers
+
+# 1-get the number from user
+# 
+# 2-cehck if positive or negative
+# 
+# 3-print the result 
+
+# num = int(input('Choose A number to check whether it\'s negative or positive: '))
+
+
+# def check_num(num):
+#   if num > 0:
+#     print(f'{num} is positive')
+#   elif num < 0:
+#     print(f'{num} is negative')
+
+#   else:
+#     print('this is Zero ya beeeeh!')
+
+# # function invokation
+
+# check_num(num)
+
+
+# num = float(input("Enter a number: "))
+# if num >= 0:
+#     if num == 0:
+#         print("Zero")
+#     else:
+#         print("Positive number")
+# else:
+#     print("Negative number")
+
+
+
+# Iterable
+#         0123
+# name = 'Heba'
+#           0       1           2          3
+# cars = ['bmw', 'ferrari', 'mercedes', 'jeep']
+
+# for letter in name:
+#   print(letter)
+
+  # # Get the sum of List
+  # numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11]
+
+  # total = 0  # 0
+
+  # for number in numbers:
+  #   # total = number + total
+  #   total += number 
+
+  # print(total)
+ 
+# tuples 
+# frinds_tuble= ('Mona', 'Ahmed', 'Salma', 'Shahd', 'Mahmoud')
+
+ # range()
+# friends = ['Mona', 'Ahmed', 'Salma', 'Shahd', 'Mahmoud']
+
+# # print(len(friends))
+# for i in range(len(friends)):
+#   print(i, friends[i])
+
+
+
+# Dictionary
+# key : walue
+# word: 'meaning'
+
+# friends = ['Mona', 'Ahmed', 'Salma', 'Shahd', 'Mahmoud']
+# # print(friends[1])
+
+
+# person1 = {
+#   'name': 'Hossam',
+#   'age': 31,
+#   'is_married': False,
+#   'profession': 'Full-stack Web Developer'
+# }
+
+# person2 = {'name': 'Heba','age': 12,'is_married': False,'profession': 'Full Awi'}
+
+
+# # print(person1['name'], person2['is_married'])
+# for i,j in person1.items():
+#   print(f'{i} | {j}')
+
+## While Loop
+
+## program that takes a number from user and returns the natural مجموع الارقام الصحيحة
+
+number = float(input('Choose a number ya 3am el 7ag: '))
+
+# [1,2,3,4,5, .....]
+sum = 0
+
+
+iterator = 0
+while iterator <= number: #True
+  sum +=iterator
+  iterator += .5
+
+print(sum)
+
+
+
+
